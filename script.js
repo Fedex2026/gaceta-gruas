27cf8e92caa0a012943c5b5ef2f6e94b0be26d08
Fedex2026 <gruasmetro@gmail.com>
2026-03-30 16:13:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 const tarifas = {
+
 A: {
-banderazo: 1200,
-km: 45,
-maniobra: 350,
-pension: 150
+banderazo: 798.02,
+km: 28.41,
+maniobra: 1640.78
 },
+
 B: {
-banderazo: 1800,
-km: 55,
-maniobra: 450,
-pension: 200
+banderazo: 916.85,
+km: 31.12,
+maniobra: 2017.65
 },
+
 C: {
-banderazo: 2500,
-km: 65,
-maniobra: 550,
-pension: 250
+banderazo: 1089.46,
+km: 35.43,
+maniobra: 2300.63
 },
+
 D: {
-banderazo: 3200,
-km: 75,
-maniobra: 650,
-pension: 300
+banderazo: 1337.08,
+km: 48.83,
+maniobra: 3172.21
 }
+
 };
+
+const ABANDERAMIENTO = 909.02;
+const ESPERA = 909.02;
+const MANUAL = 76.39;
 
 document.getElementById("btnCalcular").addEventListener("click", () => {
 
@@ -31,31 +36,19 @@ const tipo = document.getElementById("tipoGrua").value;
 const km = Number(document.getElementById("kilometros").value);
 const maniobras = Number(document.getElementById("maniobras").value);
 const dias = Number(document.getElementById("diasPension").value);
-const abanderamiento = document.getElementById("abanderamiento").value;
-const corralon = document.getElementById("corralon").value;
 
 const banderazo = tarifas[tipo].banderazo;
 const traslado = km * tarifas[tipo].km;
 const maniobra = maniobras * tarifas[tipo].maniobra;
-const pension = dias * tarifas[tipo].pension;
-
-const abander = abanderamiento === "si" ? 800 : 0;
-const corral = corralon === "si" ? 600 : 0;
 
 const total =
 banderazo +
 traslado +
-maniobra +
-pension +
-abander +
-corral;
+maniobra;
 
-document.getElementById("rBanderazo").textContent = "$" + banderazo.toLocaleString();
-document.getElementById("rTraslado").textContent = "$" + traslado.toLocaleString();
-document.getElementById("rManiobras").textContent = "$" + maniobra.toLocaleString();
-document.getElementById("rAbanderamiento").textContent = "$" + abander.toLocaleString();
-document.getElementById("rCorralon").textContent = "$" + corral.toLocaleString();
-document.getElementById("rPension").textContent = "$" + pension.toLocaleString();
-document.getElementById("rTotal").textContent = "$" + total.toLocaleString();
+document.getElementById("rBanderazo").textContent = "$" + banderazo.toFixed(2);
+document.getElementById("rTraslado").textContent = "$" + traslado.toFixed(2);
+document.getElementById("rManiobras").textContent = "$" + maniobra.toFixed(2);
+document.getElementById("rTotal").textContent = "$" + total.toFixed(2);
 
 });
